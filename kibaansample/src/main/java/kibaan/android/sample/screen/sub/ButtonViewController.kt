e9d9cb18dab2f60d1756f4c8b967115a96aef16d
Akira Nakajima <nakajima@altonotes.co.jp>
2018-12-10 18:24:27 +0900

    [ver 0.6.25] ボタン表示確認用のビューコントローラを作成

---
+++ kibaansample/src/main/java/kibaan/android/sample/screen/sub/ButtonViewController.kt
@@ -0,0 +1,49 @@
+package kibaan.android.sample.screen.sub
+
+import android.view.View
+import kibaan.android.controller.BaseViewController
+import kibaan.android.ios.IBAction
+import kibaan.android.ios.IBOutlet
+import kibaan.android.ios.UIControlState
+import kibaan.android.sample.R
+import kibaan.android.service.ScreenService
+import kibaan.android.ui.SmartButton
+
+/**
+ * ボタン確認用画面
+ */
+class ButtonViewController : BaseViewController() {
+
+    // region -> Outlets
+
+    @IBOutlet(R.id.set_image_button)
+    lateinit var setImageButton: SmartButton
+
+    // endregion
+
+    // region -> Variables
+
+    private var count = 0
+
+    // endregion
+
+    // region -> Action
+
+    @IBAction(R.id.close_button)
+    fun actionCloseButton(sender: View) {
+        ScreenService.shared.removeSubScreen()
+    }
+
+    @IBAction(R.id.set_image_change_button)
+    fun actionSetImageChangeButton(sender: View) {
+        if (count % 2 == 0) {
+            setImageButton.setImage(R.mipmap.checkbox_off, state = UIControlState.normal)
+        } else {
+            setImageButton.setImage(R.mipmap.checkbox_on, state = UIControlState.normal)
+        }
+        count++
+    }
+//    fun action
+
+    // endregion
+}
--- kibaansample/src/main/java/kibaan/android/sample/screen/top/TopViewController.kt
+++ kibaansample/src/main/java/kibaan/android/sample/screen/top/TopViewController.kt
@@ -1,6 +1,5 @@
 package kibaan.android.sample.screen.top
 
-import android.databinding.DataBindingUtil
 import android.view.View
 import kibaan.android.controller.BaseViewController
 import kibaan.android.ios.IBAction
@@ -9,6 +8,7 @@ import kibaan.android.ios.UIButton
 import kibaan.android.ios.UIControlState
 import kibaan.android.sample.R
 import kibaan.android.sample.screen.connection.ConnectionViewController
+import kibaan.android.sample.screen.sub.ButtonViewController
 import kibaan.android.sample.screen.sub.SubViewController
 import kibaan.android.sample.screen.table.SampleTableViewController
 import kibaan.android.service.ScreenService
@@ -71,6 +71,11 @@ class TopViewController : BaseViewController() {
     fun actionTableButton(sender: View) {
         Log.d(javaClass.simpleName, "actionTableButton: ${System.currentTimeMillis()}")
         ScreenService.shared.addSubScreen(SampleTableViewController::class)
+    }
+
+    @IBAction(R.id.button_button)
+    fun actionButtonButton(sender: View) {
+        ScreenService.shared.addSubScreen(ButtonViewController::class)
     }
 
     @IBAction(R.id.sub_button)
