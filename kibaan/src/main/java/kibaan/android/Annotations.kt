3c6fad07403f879eef973b383741fbf7e24cb0bf
Akira Nakajima <nakajima@altonotes.co.jp>
2018-12-13 11:22:24 +0900

    [ver 0.6.31] kotlinのバージョンを「1.3.11」にアップデート

--- kibaan/src/main/java/kibaan/android/Annotations.kt
+++ kibaan/src/main/java/kibaan/android/Annotations.kt
@@ -5,4 +5,5 @@ package kibaan.android
  * Created by yamamoto on 2018/04/13.
  */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.EXPRESSION, AnnotationTarget.PROPERTY, AnnotationTarget.LOCAL_VARIABLE)
+@Retention(AnnotationRetention.SOURCE)
 annotation class AndroidUnique
