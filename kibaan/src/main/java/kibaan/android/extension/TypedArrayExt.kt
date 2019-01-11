1671d4a31d6dda2331501b74ec30ca996ce7a224
佐藤 雅輝 <ma_sato@cross-c.jo.jp>
2019-01-11 10:50:17 +0900

    extensionコメント追加とUTの追加

--- kibaan/src/main/java/kibaan/android/extension/TypedArrayExt.kt
+++ kibaan/src/main/java/kibaan/android/extension/TypedArrayExt.kt
@@ -2,6 +2,9 @@ package kibaan.android.extension
 
 import android.content.res.TypedArray
 
+/**
+ * 引数に指定した属性が存在する場合はStringとして取得し、存在しな場合はnullを返す.
+ */
 fun TypedArray.getStringOrNull(id: Int): String? {
     return if (hasValue(id)) getString(id) else null
 }
