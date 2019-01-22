80e7a492344ec059354974ee09a9222394e928a9
佐藤 雅輝 <ma_sato@cross-c.jo.jp>
2019-01-22 15:05:32 +0900

    UIPageControlの微調整とコメント修正

--- kibaan/src/main/java/kibaan/android/ios/UIPageControl.kt
+++ kibaan/src/main/java/kibaan/android/ios/UIPageControl.kt
@@ -9,14 +9,17 @@ import kibaan.android.ui.EllipseButton
 import kibaan.android.ui.SmartButton
 import kibaan.android.util.DeviceUtils
 
+/**
+ * ページ数と選択中のページを表示するUI
+ */
 class UIPageControl : LinearLayout {
 
     // region -> Constants
 
-    private var leftMargin = DeviceUtils.toDp(context, 60)
-    private var buttonSize = DeviceUtils.toDp(context, 50)
-    private var normalDefaultColor = UIColor(rgbValue = 0xDEDEDE)
-    private var selectedDefaultColor = UIColor(rgbValue = 0x555555)
+    private var spacing = DeviceUtils.toDp(context, 70)
+    private var buttonSize = DeviceUtils.toDp(context, 60)
+    private var defaultNormalColor = UIColor(rgbValue = 0xDEDEDE)
+    private var defaultSelectedColor = UIColor(rgbValue = 0x555555)
 
     // endregion
 
@@ -29,27 +32,29 @@ class UIPageControl : LinearLayout {
 
     var onPageChanged: ((UIPageControl) -> Unit)? = null
 
-    /* numberOfPagesを呼ばないとbuttonを生成することができない */
-    /* PageControlのページ数を設定する */
+    /**
+     * ページ数
+     * numberOfPagesを呼ばないとbuttonを生成することができない
+     */
     var numberOfPages: Int by didSet(0) {
         constructButtons()
     }
 
-    /* 現在のページ */
+    /** 現在のページ */
     var currentPage: Int by didSet(0) {
         (0 until childCount).forEach { index ->
             getChildAt(index).isSelected = currentPage == index
         }
     }
 
-    /* 選択されていないページの色を設定する */
-    var pageIndicatorTintColor: UIColor by didSet(normalDefaultColor) {
+    /** 選択されていないページの色を設定する */
+    var pageIndicatorTintColor: UIColor by didSet(defaultNormalColor) {
         (0 until childCount).mapNotNull { getChildAt(it) as? SmartButton }.forEach {
             it.setBackgroundColor(pageIndicatorTintColor, UIControlState.normal)
         }
     }
 
-    /* 選択されているページの色を設定する */
+    /** 選択されているページの色を設定する */
     var currentPageIndicatorTintColor: UIColor by didSet(UIColor.clear) {
         (0 until childCount).mapNotNull { getChildAt(it) as? SmartButton }.forEach {
             it.setBackgroundColor(currentPageIndicatorTintColor, UIControlState.selected)
@@ -80,13 +85,13 @@ class UIPageControl : LinearLayout {
             pageIndicatorTintColor = UIColor(
                 array.getColor(
                     R.styleable.UIPageControl_pageIndicatorTintColor,
-                    normalDefaultColor.intValue
+                    defaultNormalColor.intValue
                 )
             )
             currentPageIndicatorTintColor = UIColor(
                 array.getColor(
                     R.styleable.UIPageControl_currentPageIndicatorTintColor,
-                    selectedDefaultColor.intValue
+                    defaultSelectedColor.intValue
                 )
             )
             array.recycle()
@@ -94,19 +99,22 @@ class UIPageControl : LinearLayout {
     }
     // endregion
 
-    /* pageControlButtonを生成・設定する */
+    /**
+     *  pageControlButtonを生成・設定する
+     */
     private fun constructButtons() {
         removeAllViews()
         (0 until numberOfPages).forEach { offset ->
             val button = EllipseButton(context)
             val layoutParams = LinearLayout.LayoutParams(buttonSize, buttonSize)
-            val leftMargin = if (offset != 0) this.leftMargin else 0
+            val leftMargin = if (offset != 0) this.spacing else 0
             layoutParams.setMargins(leftMargin, 0, 0, 0)
             button.layoutParams = layoutParams
             if (button.backgroundColor(forState = UIControlState.normal) == null) {
-                button.setBackgroundColor(normalDefaultColor, forState = UIControlState.normal)
+                button.setBackgroundColor(defaultNormalColor, forState = UIControlState.normal)
             }
-            button.setBackgroundColor(selectedDefaultColor, forState = UIControlState.selected)
+            button.setBackgroundColor(defaultSelectedColor, forState = UIControlState.selected)
+            // FIXME: iOSのUIPageControlは丸がButtonになっておらず、右半分をタップすると右のページに進み、左半分をタップすると左のページに進む
             button.setOnClickListener {
                 currentPage = offset
                 onPageChanged?.invoke(this)
