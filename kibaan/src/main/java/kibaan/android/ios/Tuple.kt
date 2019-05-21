8f344706cf0de3801fefef8ca9395761dab83196
Keita Yamamoto <yamamoto@altonotes.co.jp>
2019-05-21 20:05:14 +0900

    Tupleの真似事を追加

---
+++ kibaan/src/main/java/kibaan/android/ios/Tuple.kt
@@ -0,0 +1,8 @@
+package kibaan.android.ios
+
+val <A, B> Pair<A, B>._0: A get() = first
+val <A, B> Pair<A, B>._1: B get() = second
+
+val <A, B, C> Triple<A, B, C>._0: A get() = first
+val <A, B, C> Triple<A, B, C>._1: B get() = second
+val <A, B, C> Triple<A, B, C>._2: C get() = third
