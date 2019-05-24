89f885f204474307b9fb3397fa35b466b1bfca34
Akira Nakajima <nakajima@altonotes.co.jp>
2019-05-24 17:24:47 +0900

    DoubleExtに「truncatingRemainder」を追加

--- kibaan/src/main/java/kibaan/android/extension/DoubleExt.kt
+++ kibaan/src/main/java/kibaan/android/extension/DoubleExt.kt
@@ -1,9 +1,18 @@
 package kibaan.android.extension
 
 import kibaan.android.ios.CGFloat
+import kotlin.math.floor
 
 /**
  * CGFloatに変換して返す
  */
 val Double.cgFloatValue: CGFloat
     get() = this
+
+/**
+ * 余りを返す
+ */
+fun Double.truncatingRemainder(dividingBy: Double) : Double {
+    val q: Double = floor((this / dividingBy))
+    return this - dividingBy * q
+}
