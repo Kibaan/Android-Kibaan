b80348e894c78c781809742c180e9712bd3c6cf3
Keita Yamamoto <yamamoto@altonotes.co.jp>
2019-09-27 14:27:33 +0900

    NSDecimalNumberの丸め処理の修正

--- kibaan/src/test/java/kibaan/android/extension/NSDecimalNumberExtTest.kt
+++ kibaan/src/test/java/kibaan/android/extension/NSDecimalNumberExtTest.kt
@@ -9,21 +9,39 @@ class NSDecimalNumberExtTest {
     @Test
     fun testRoundDown() {
         val decimal = NSDecimalNumber(string = "1.98765")
+
+        assertEquals("1.98765", decimal.stringValue)
         assertEquals("1", decimal.roundDown().stringValue)
         assertEquals("1.9", decimal.roundDown(1).stringValue)
         assertEquals("1.98", decimal.roundDown(2).stringValue)
-
-        assertEquals("1.98765", decimal.stringValue)
+        assertEquals("-1", NSDecimalNumber(string = "-0.1").roundDown().stringValue)
     }
 
     @Test
     fun testRoundUp() {
         val decimal = NSDecimalNumber(string = "1.12345")
+        assertEquals("1.12345", decimal.stringValue)
+
         assertEquals("2", decimal.roundUp().stringValue)
         assertEquals("1.2", decimal.roundUp(1).stringValue)
         assertEquals("1.13", decimal.roundUp(2).stringValue)
+        assertEquals("0", NSDecimalNumber(string = "-0.1").roundUp().stringValue)
+    }
 
-        assertEquals("1.12345", decimal.stringValue)
+    @Test
+    fun testRoundAbsDown() {
+        assertEquals("0", NSDecimalNumber(string = "0.1").roundAbsDown().stringValue)
+        assertEquals("1.9", NSDecimalNumber(string = "1.98").roundAbsDown(1).stringValue)
+        assertEquals("0", NSDecimalNumber(string = "-0.1").roundAbsDown().stringValue)
+        assertEquals("-1", NSDecimalNumber(string = "-1.9").roundAbsDown().stringValue)
+    }
+
+    @Test
+    fun testRoundAbsUp() {
+        assertEquals("1", NSDecimalNumber(string = "0.1").roundAbsUp().stringValue)
+        assertEquals("2.0", NSDecimalNumber(string = "1.91").roundAbsUp(1).stringValue)
+        assertEquals("-1", NSDecimalNumber(string = "-0.1").roundAbsUp().stringValue)
+        assertEquals("-2", NSDecimalNumber(string = "-1.1").roundAbsUp().stringValue)
     }
 
     @Test
